560c1e2b76bc7a71e6ae192e2337875684c1cdc5
young0306 <150253108+young0306@users.noreply.github.com>
2025-09-27 03:08:50 +0900

    0927-Inventory
    틀만

---
+++ forgotten journey/Assets/Scripts/InventoryUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    bool activeInventory = false;
+
+    void Start()
+    {
+        inventoryPanel.SetActive(activeInventory);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+    }
+}
